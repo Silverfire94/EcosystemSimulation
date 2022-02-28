b47cb99bea496d2f7a2f5add62f9e08a71401e28
R0V0 <43351781+R0V0@users.noreply.github.com>
2022-02-28 21:15:34 +0000

    Add files via upload

---
+++ Eagles/Utililty/Time.java
@@ -0,0 +1,32 @@
+package Utililty;
+/**
+ * Write a description of class Time here.
+ *
+ * @author (your name)
+ * @version (a version number or a date)
+ */
+public class Time
+{
+    // instance variables - replace the example below with your own
+    private int x;
+
+    /**
+     * Constructor for objects of class Time
+     */
+    public Time()
+    {
+        
+    }
+
+    /**
+     * 
+     */
+    public static boolean isDay(int dayLengthInSteps, int steps)
+    {
+        return (steps/dayLengthInSteps + 1) % 2 > 0;
+    }
+    
+    public static boolean isDay2(int dayLengthInStep , int steps){
+        return (steps % dayLengthInStep == 0);
+    }
+}
